a37f37ad20427c501e8eddfd2192e9817eba6e69
JERICK UBALDO <202411452@gordoncollege.edu.ph>
2026-05-14 06:23:42 +0000

    it doesnt look the same as in the logo

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -62,8 +62,10 @@ export function Sidebar() {
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
           >
-            <rect x="0" y="0" width="32" height="32" rx="8" fill="white" />
+            <rect x="0" y="0" width="32" height="32" rx="9" fill="white" />
             <path
+              fillRule="evenodd"
+              clipRule="evenodd"
               d="M16 6C12.6863 6 10 8.68629 10 12C10 16.5 16 26 16 26C16 26 22 16.5 22 12C22 8.68629 19.3137 6 16 6ZM16 14.5C14.6193 14.5 13.5 13.3807 13.5 12C13.5 10.6193 14.6193 9.5 16 9.5C17.3807 9.5 18.5 10.6193 18.5 12C18.5 13.3807 17.3807 14.5 16 14.5Z"
               fill="black"
             />
