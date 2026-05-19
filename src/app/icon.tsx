263eb0db6e049bee3715dd240a654e61b56e04e9
JERICK UBALDO <202411452@gordoncollege.edu.ph>
2026-05-19 09:49:45 +0000

    i want the logo look exactly like this

--- src/app/icon.tsx
+++ src/app/icon.tsx
@@ -23,14 +23,18 @@ export default function Icon() {
         }}
       >
         <svg
-          width="22"
-          height="22"
+          width="24"
+          height="24"
           viewBox="0 0 24 24"
-          fill="black"
+          fill="none"
+          stroke="black"
+          strokeWidth="2.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
           xmlns="http://www.w3.org/2000/svg"
         >
-          <path d="M12 21s-7-7.5-7-12a7 7 0 1 1 14 0c0 4.5-7 12-7 12Z" />
-          <circle cx="12" cy="9" r="2.5" fill="white" />
+          <path d="M20 10c0 6-8 12-8 12s-8-6-8-12a8 8 0 0 1 16 0Z" />
+          <circle cx="12" cy="10" r="2.5" />
         </svg>
       </div>
     ),
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -97,11 +97,15 @@ export default function LoginPage() {
             width="48"
             height="48"
             viewBox="0 0 24 24"
-            fill="black"
+            fill="none"
+            stroke="black"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             xmlns="http://www.w3.org/2000/svg"
           >
-            <path d="M12 21s-7-7.5-7-12a7 7 0 1 1 14 0c0 4.5-7 12-7 12Z" />
-            <circle cx="12" cy="9" r="2.5" fill="white" />
+            <path d="M20 10c0 6-8 12-8 12s-8-6-8-12a8 8 0 0 1 16 0Z" />
+            <circle cx="12" cy="10" r="2.5" />
           </svg>
         </div>
         <div>
