00129f2d616755fdb2c1a2fb8b37936d312684f2
JERICK UBALDO <202411452@gordoncollege.edu.ph>
2026-05-19 09:58:27 +0000

    for password textbox keep it empty

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -58,15 +58,10 @@ export function Sidebar() {
             width="24"
             height="24"
             viewBox="0 0 24 24"
-            fill="none"
-            stroke="black"
-            strokeWidth="2.5"
-            strokeLinecap="round"
-            strokeLinejoin="round"
+            fill="black"
             xmlns="http://www.w3.org/2000/svg"
           >
-            <path d="M20 10c0 6-8 12-8 12s-8-6-8-12a8 8 0 0 1 16 0Z" />
-            <circle cx="12" cy="10" r="2.5" />
+            <path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z" />
           </svg>
         </div>
         <span className={cn(
